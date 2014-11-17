ba081e790b63aeedc2bbb581c316ab3506b490a6
Travis Flatt <tflatt74@gmail.com>
2014-11-16 18:06:18 -0600

    zombie, tests added

--- mockingbird.js
+++ mockingbird.js
@@ -31,6 +31,14 @@ app.get('/about', function(req, res) {
 	 } );
 });
 
+app.get('/tours/hood-river', function(req, res) {
+	res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res) {
+	res.render('tours/request-group-rate');
+});
+
 // custom 404 page
 app.use(function(req, res) {
 	res.status(404);
